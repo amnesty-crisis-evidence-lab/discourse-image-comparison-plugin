b40ce9c6dc0c01a340089b82d3b69a956cff9409
Tuan Ngo <tuan@trexanhlab.com>
2016-12-07 09:04:17 +0700

    include css in plugin

---
+++ plugin.rb
@@ -0,0 +1,7 @@
+# name: image-comparison
+# about: zoom tile in image comparison
+# version: 0.2
+# authors: Amnesty dev team
+
+register_asset 'stylesheets/style.scss'
+register_asset 'javascripts/q.min.js'
